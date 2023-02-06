75cf2f5f3857046533acb920b0f3bb53197b942f
Alex <alexandroff1997@gmail.com>
2023-02-06 20:05:14 +0700

    storage service: implementing the logic for storing and retrieving a token

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import { getArgs } from './helpers/args.js';
 import { printError, prinSuccess, printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан токен');
+		return;
+	}
+	try {
+		await saveKeyValue('token', token);
+		prinSuccess('Токен сохранен');
+	} catch (err) {
+		printError(err.message);
+	}
+};
 
 const startCli = () => {
 	const args = getArgs(process.argv);
@@ -13,7 +27,7 @@ const startCli = () => {
 	}
 
 	if (args.t) {
-
+		return saveToken(args.t);
 	}
 };
 
---
+++ services/storage.service.js
@@ -0,0 +1,47 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const readFileFunc = async () => {
+	const file = await promises.readFile(filePath);
+
+	return JSON.parse(file)
+}
+ 
+const saveKeyValue = async (key, value) => {
+	let data = {};
+
+	if (await isExist(filePath)) {
+		data = await readFileFunc();
+	}
+	
+	data[key] = value;
+	await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+	let data = {};
+
+	if (await isExist(filePath)) {
+		data = await readFileFunc();
+		return data[key];
+	}
+	
+	return null;
+}
+
+const isExist = async (path) => {
+	try {
+		await promises.stat(path);
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+export { 
+	saveKeyValue,
+	getKeyValue, 
+}
